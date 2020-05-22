211f4c51921ab7538a592c76d22d26faf9742522
Tosin <tosin@sourcebreaker.com>
2020-05-22 18:55:20 +0100

    add import to task module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { TasksController } from './tasks.controller';
-import { TasksService } from './tasks.service';
+import { TaskController } from './tasks.controller';
+import { TaskService } from './tasks.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TaskSchema } from './schema/task.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  controllers: [TasksController],
-  providers: [TasksService],
+  controllers: [TaskController],
+  providers: [TaskService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Task', schema: TaskSchema },
+    ]),
+    AuthModule,
+  ],
 })
-export class TasksModule {}
+export class TasksModule { }
